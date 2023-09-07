794104e90e13855cc8b4600f7d5c5645be5873b9
Miguel Guevara <guesian.05.88@gmail.com>
2023-09-07 00:27:35 -0500

    added responve button

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  console.log('que paso')
   const aside = document.querySelector(".layout__aside");
   const btn = document.querySelector(".layout__menu-toggle");
-  console.log(btn)
 
   btn.addEventListener("click", (e) => {
-    console.log('clik')
+    const iconBars = document.querySelector(".fa-bars");
+    const iconXmark = document.querySelector(".fa-xmark");
+
     const visible = document.querySelector(".layout__aside--visible");
     if (!visible) {
       aside.classList.add("layout__aside--visible");
+      console.log(iconXmark);
+      iconBars.style.opacity = 0;
+      iconXmark.style.opacity = 1;
+    } else {
+      aside.classList.remove("layout__aside--visible");
+      iconBars.style.opacity = 1;
+      iconXmark.style.opacity = 0;
     }
   });
 });
