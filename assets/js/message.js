ebf7234763ba0057b850c80a2fe10c6a077ebbb4
Miguel Guevara <guesian.05.88@gmail.com>
2023-09-07 23:57:17 -0500

    notification message__sent

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,22 +1,50 @@
+const btn = document.getElementById("button");
+const message = document.getElementById("message");
+const form = document.getElementById("form");
 
-const btn = document.getElementById('button');
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  btn.value = "Sending...";
 
-   btn.value = 'Sending...';
+  const serviceID = "default_service";
+  const templateID = "template_0tbm55e";
+  const message__sent = document.createElement("label");
 
-   const serviceID = 'default_service';
-   const templateID = 'template_0tbm55e';
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Enviar mensaje";
 
-  emailjs.sendForm(serviceID, templateID, this)
-  .then(() => {
-    btn.value = 'Enviar mensaje';
-    event.target.reset()
-    
-  }, (err) => {
-    btn.value = 'Send Email';
-    alert(JSON.stringify(err));
-  })
+      event.target.reset();
+      message__sent.textContent = "Mensaje enviado";
+      message__sent.className = "message__sent"
+      message.appendChild(message__sent);
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
+
+
+
+  form.addEventListener("focus", (e) => {
+    if ( form.contains(message)) {
+      console.log('estoy en el input')
+    }
+  });
+
+
+
+const inputs = form.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", (e) => {
+    const message__sent = form.querySelector(".message__sent")
+    if ( message.childElementCount) {
+      console.log('soy yo')
+      message__sent.remove()
+    }
+  });
 });
